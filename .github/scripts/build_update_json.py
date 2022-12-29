ce0d6193f1432955d6ca2b4ee647a9578329b684
euvbonk <seiste@yahoo.de>
2022-12-29 12:56:56 +0100

    Only reformatting according to PEP8 E501 (line to long)

--- .github/scripts/build_update_json.py
+++ .github/scripts/build_update_json.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 """
-Creates a json file with information about all datasheet files in given directory path.
-The output file, named '{}' by default, is also stored into given directory path.
+Creates a json file with information about all datasheet files in
+given directory path.
+The output file, named '{}' by default, is also stored into
+given directory path.
 
 Examples:
    {} path/to/directory
@@ -25,30 +27,44 @@ DEFAULT_FILE_NAME = 'bahnhof.json'
 if __name__ == '__main__':
     script_name = os.path.basename(sys.argv[0])
     args_parser = argparse. ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
         description=textwrap.dedent(__doc__.format(DEFAULT_FILE_NAME,
                                                    script_name,
-                                                   script_name)))
-    args_parser.add_argument('ds_files', metavar='path/to/directory', nargs=1,
-                             help='Directory where files are stored')
-    args_parser.add_argument('-o', '--out-file', metavar='file-name', nargs='?',
-                             required=False, default=DEFAULT_FILE_NAME,
-                             help='File where result is stored to (default: "{}")'.format(DEFAULT_FILE_NAME))
+                                                   script_name)),
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    args_parser.add_argument(
+        'ds_files',
+        metavar='path/to/directory',
+        nargs=1,
+        help='Directory where files are stored')
+    args_parser.add_argument(
+        '-o',
+        '--out-file',
+        metavar='file-name',
+        nargs='?',
+        required=False,
+        default=DEFAULT_FILE_NAME,
+        help='File where result is stored to (default: "{}")'.format(
+            DEFAULT_FILE_NAME))
     args = args_parser.parse_args()
 
     # provide given file as resolved absolute path
     ds_file = pathlib.Path(args.ds_files[0]).resolve()
 
     if not ds_file.exists():
-        args_parser.error('Given path "{}" does not exists.'.format(args.ds_files[0]))
+        args_parser.error(
+            'Given path "{}" does not exists.'.format(args.ds_files[0]))
 
     # given path must exist and a folder
     if not ds_file.is_dir():
-        args_parser.error('Given path "{}" is not a folder!'.format(args.ds_files[0]))
+        args_parser.error(
+            'Given path "{}" is not a folder!'.format(args.ds_files[0]))
 
     _working_directory = ds_file.absolute()
-    # outfile is stored in given directory; check if suffix json is present, add if necessary
-    out_file = pathlib.Path(_working_directory, args.out_file).with_suffix('.json')
+    # outfile is stored in given directory; check if suffix json is present,
+    # add if necessary
+    out_file = pathlib.Path(_working_directory,
+                            args.out_file).with_suffix('.json')
 
     all_in_one = OrderedDict()
     for ds_element in sorted(_working_directory.glob('*.xml')):
